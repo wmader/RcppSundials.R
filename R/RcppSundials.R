d24e504216025065dc113c1c103d847ff25233da
Wolfgang Mader <Wolfgang.Mader@fdm.uni-freiburg.de>
2016-11-02 11:29:17 +0100

    Clean up package documentation
    DESCRIPTION now uses person(). Added myself as contributer.
    RcppSundials-package.Rd now roxygenized from RcppSundials.R.

---
+++ R/RcppSundials.R
@@ -0,0 +1,9 @@
+#' Sundials for R
+#'
+#' Wrapper around Sundials numerical library using Rcpp and RcppArmadillo
+#'
+#' Sundials is a SUite of Nonlinear and DIfferential/ALgebraic
+#' Equation Solvers. This packages makes the solver cvodes and ida available to
+#' R. cvodes is a solver for stiff and nonstiff systems of ordinary linear
+#' equations, ida solves differential-algebraic equation systems.
+"_PACKAGE"
